037810a8de484eb56da199e7b218e1ab7c9e854b
OlafKoerner <59175692+OlafKoerner@users.noreply.github.com>
2024-12-06 21:40:20 +0100

    fixing

--- htdocs/js/wui.js
+++ htdocs/js/wui.js
@@ -638,11 +638,11 @@ vz.wui.handleControls = function(action, keepPeriodStartFixed) {
 		}
 
 	function set_time_frame(control) {
-		let ts_now = new Date();
-		alert(ts_now);
-		let dt_now = ts_now.getDate() + "T" + ts_now.getTime();
-		alert(dt_now);
-		let dt_start = prompt(button_str + control + "\n\nstart (YYYY-MM-DDThh:mm:ss): ", dt_now);
+		//let ts_now = new Date();
+		//alert(ts_now);
+		//let dt_now = ts_now.getDate() + "T" + ts_now.getTime();
+		//alert(dt_now);
+		let dt_start = prompt(button_str + control + "\n\nstart (YYYY-MM-DDThh:mm:ss): ", "2024-09-01T08:00:00");
 		let dt_end   = prompt(button_str + control + "\n\nend (YYYY-MM-DDThh:mm:ss): ", dt_start);
 		//confirm('time interval from ' + dt_start + ' till ' + dt_end);
 		const ts_start = Date.parse(dt_start);
