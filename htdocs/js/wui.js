78df32eaa8d164ae91f5e6192f9f530b5a464d41
OlafKoerner <59175692+OlafKoerner@users.noreply.github.com>
2024-12-06 21:20:03 +0100

    set_time_frame:  add Date.now

--- htdocs/js/wui.js
+++ htdocs/js/wui.js
@@ -638,12 +638,12 @@ vz.wui.handleControls = function(action, keepPeriodStartFixed) {
 		}
 
 	function set_time_frame(control) {
-		let dt_start = prompt(button_str + control + "\n\nstart (YYYY-MM-DDThh:mm:ss): ", "2024-09-01T08:00:00");
+		let dt_start = prompt(button_str + control + "\n\nstart (YYYY-MM-DDThh:mm:ss): ", new Date(Date.now()));
 		let dt_end   = prompt(button_str + control + "\n\nend (YYYY-MM-DDThh:mm:ss): ", dt_start);
 		//confirm('time interval from ' + dt_start + ' till ' + dt_end);
 		const ts_start = Date.parse(dt_start);
 		const ts_end   = Date.parse(dt_end);
-		alert(toString(ts_start) + toString(ts_end));
+		alert(ts_start.toString() + ts_end).toString());
 		vz.wui.zoom(ts_start, ts_end);
 	}
 
