68add833b83fb1cc25743c99d7115d6b236ebc4b
OlafKoerner <59175692+OlafKoerner@users.noreply.github.com>
2024-12-07 22:20:46 +0100

    fixing

--- restapi/restapi_bottle.py
+++ restapi/restapi_bottle.py
@@ -73,8 +73,8 @@ def logbook_add(device_id=0, command_str='', ts_min=0, ts_max=0, status_str=''):
             'command' : command_str,
             'min timestamp' : ts_min, 
             'max timestamp' : ts_max, 
-            'min datetime' : datetime.fromtimestamp(ts_min/1000), 
-            'max datetime' : datetime.fromtimestamp(ts_max/1000), 
+            'min datetime' : datetime.fromtimestamp(ts_min/1000).astimezone().isoformat(), 
+            'max datetime' : datetime.fromtimestamp(ts_max/1000).astimezone().isoformat(), 
             'status' : status_str
             })
 
