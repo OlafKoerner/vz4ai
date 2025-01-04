05bc06e5bab500a6cbf4b100c43091f12a754f33
OlafKoerner <59175692+OlafKoerner@users.noreply.github.com>
2025-01-04 21:48:43 +0100

    fixing global var dh

--- restapi/restapi_bottle.py
+++ restapi/restapi_bottle.py
@@ -214,13 +214,14 @@ def get_identified_devices(ts_from_str, ts_to_str, window_length_str): # -> dict
 
 @app.route('/goto_event/<device_id_str>/<event_id_str>', method=['GET'], name='goto_event')
 def get_event_timeframe(device_id_str, event_id_str):
-    if global dh == 0:
+    global dh
+    if dh == 0:
         try:
-            global dh = ClassPowerAIDataHandler(".env")
-            global dh.read_events_from_db()
+            dh = ClassPowerAIDataHandler(".env")
+            dh.read_events_from_db()
         except:
             logging.error('Could not create ClassPowerAIDataHandler! Wrong or missing .env-file?')
-    event = global dh.event_list[int(device_id_str)][int(event_id_str) - 1]
+    event = dh.event_list[int(device_id_str)][int(event_id_str) - 1]
     response = {'ts_min' : event[0], 'ts_max' : event[-1]}
     logging.info(f'Go to timeframe: {response}')
     return response
