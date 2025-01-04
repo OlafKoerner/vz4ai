565eb831235467c55ddf52ee96c90ed871e2a0b0
OlafKoerner <59175692+OlafKoerner@users.noreply.github.com>
2025-01-04 21:34:34 +0100

    add logging info for goto_event

--- restapi/restapi_bottle.py
+++ restapi/restapi_bottle.py
@@ -221,6 +221,7 @@ def get_event_timeframe(device_id_str, event_id_str):
             logging.error('Could not create ClassPowerAIDataHandler! Wrong or missing .env-file?')
     event = dh.event_list[int(device_id_str)][int(event_id_str) - 1]
     response = {'ts_min' : event[0], 'ts_max' : event[-1]}
+    logging.info(f'Go to timeframe: {response}')
     return response
 
 
