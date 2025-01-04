b748f6dffed18cbd5a0b957fea756ed6380f8460
OlafKoerner <59175692+OlafKoerner@users.noreply.github.com>
2025-01-04 17:33:42 +0100

    fixing device/event_id_str at route goto_event()

--- restapi/restapi_bottle.py
+++ restapi/restapi_bottle.py
@@ -219,7 +219,7 @@ def get_event_timeframe(device_id_str, event_id_str):
             dh.read_events_from_db()
         except:
             logging.error('Could not create ClassPowerAIDataHandler! Wrong or missing .env-file?')
-    event = dh.event_list[device_id][event_id]
+    event = dh.event_list[int(device_id_str)][int(event_id_str) - 1]
     response = {'ts_min' : event[0], 'ts_max' : event[-1]}
     return response
 
