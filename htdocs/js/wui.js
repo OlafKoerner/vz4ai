12d2590a143812e76e286100580b50da25f18398
OlafKoerner <59175692+OlafKoerner@users.noreply.github.com>
2025-02-09 18:12:43 +0100

    again

--- htdocs/js/wui.js
+++ htdocs/js/wui.js
@@ -1007,25 +1007,25 @@ vz.wui.refresh = function() {
 
 	/* OKO function to write device ID to database via REST API (bottle) */
 	async function read_pi_disk_usage() {       
-			try 
-			{ 
-					const response = await fetch(url_rest_api + 'diskspace', { 
-									/*mode: "cors",  https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#supplying_request_options  */
-									method: "GET",
-									headers: {
-											'Accept': 'application/json',
-											'Content-Type': 'application/json'
-									},
-									//OKO not working for old iPhone: signal: AbortSignal.timeout(5000) /* https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal#aborting_a_fetch_operation_with_a_timeout  */
-							}
-					)
-					const text = await response.text()
-					return text;
-			} 
-			catch(err) 
-			{ 
-					alert(`Error: ${err.name}, ${err.message}.\nRaspberryPi not reachable. Restart REST-API (bottle) with:\n$ python3 my_bottle_restapi.py &`); 
-			} 
+		try 
+		{ 
+				const response = await fetch(url_rest_api + 'diskspace', { 
+								/*mode: "cors",  https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#supplying_request_options  */
+								method: "GET",
+								headers: {
+										'Accept': 'application/json',
+										'Content-Type': 'application/json'
+								},
+								//OKO not working for old iPhone: signal: AbortSignal.timeout(5000) /* https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal#aborting_a_fetch_operation_with_a_timeout  */
+						}
+				)
+				const text = await response.text()
+				return text;
+		} 
+		catch(err) 
+		{ 
+				alert(`Error: ${err.name}, ${err.message}.\nRaspberryPi not reachable. Restart REST-API (bottle) with:\n$ python3 my_bottle_restapi.py &`); 
+		} 
 	}
 	//OKO draw current diskspace
 	read_pi_disk_usage().then( text => { 
